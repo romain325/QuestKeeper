537b90bb7ad85fd626b3bdc6f948466d3de833c5
AugusteG <auguste.gargouil@gmail.com>
2023-02-24 13:54:12 +0100

    ui
    ui

--- src/model/User.php
+++ src/model/User.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"]."src/service/PlayerService.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/src/service/PlayerService.php";
 
 class User
 {
--- src/service/PlayerService.php
+++ src/service/PlayerService.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"]."src/model/Player.php";
+require_once $_SERVER["DOCUMENT_ROOT"]."/src/model/Player.php";
 include_once "AuthService.php";
 
 function getCurrentPlayer() : Player|null {
